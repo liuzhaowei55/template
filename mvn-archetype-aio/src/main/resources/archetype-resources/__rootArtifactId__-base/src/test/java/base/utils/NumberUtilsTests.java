2734153a20cf7c3a19317eb9e4cf91253a04a347
where.liu <where.liu@huolala.cn>
2022-03-28 13:56:55 +0800

    feat: mvn-archetype-aio

--- mvn-archetype-aio/src/main/resources/archetype-resources/__rootArtifactId__-base/src/test/java/base/utils/NumberUtilsTests.java
+++ mvn-archetype-aio/src/main/resources/archetype-resources/__rootArtifactId__-base/src/test/java/base/utils/NumberUtilsTests.java
@@ -1,7 +1,7 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-package ${package}.utils;
+package ${package}.base.utils;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
---
+++ mvn-archetype-aio/src/main/resources/archetype-resources/__rootArtifactId__-infrastructure/src/main/java/infrastructure/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.infrastructure.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("${package}.infrastructure.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+    }
+}
